03cd08f057ae1b5be4d2f1a6d20f6853449dfa6a
MSUgamer576 <73747315+MSUgamer576@users.noreply.github.com>
2021-02-25 20:26:03 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('1234.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('123.jpg')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(3)
